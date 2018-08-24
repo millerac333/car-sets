eaa286a6dcc5368a2b57fbc4e6edf63540f7d93c
Aaron Miller <millerac333@gmail.com>
2018-08-24 14:12:52 -0500

    finished part one of excercise

--- Program.cs
+++ Program.cs
@@ -1,9 +1,4 @@
-﻿// Instructions
-// Create another set named UsedLot and add two more car model strings to it.
-// Use the UnionWith() method on Showroom to add in the two models you added to UsedLot.
-// You've sold one of your cars. Remove it from the set with the Remove() method.
-
-// Acquiring more cars
+﻿// Acquiring more cars
 // Now create another HashSet of cars in a variable Junkyard. Someone who owns a junkyard full of old cars has approached you about buying the entire inventory. In the new set, add some different cars, but also add a few that are the same as in the Showroom set.
 // Create a new HashSet of your show room with HashSet<string> clone = new HashSet<string>(Showroom);
 // Use the IntersectWith() method on the clone to see which cars exist in both the show room and the junkyard.
@@ -40,6 +35,13 @@ namespace sets
             HashSet<string> UsedLot = new HashSet<string>();
             UsedLot.Add("Silverado");
             UsedLot.Add("Pathfinder");
+
+            // Use the UnionWith() method on Showroom to add in the two models you added to UsedLot.
+            Showroom.UnionWith(UsedLot);
+            Console.WriteLine($"{Showroom}");
+
+            // You've sold one of your cars. Remove it from the set with the Remove() method.
+            Showroom.Remove("Challenger");
         }
     }
 }
